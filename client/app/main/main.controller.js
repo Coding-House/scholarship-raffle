cbc8421b1686a0c37afa529eeeacc1cbd45968a2
Codective <codective@gmail.com>
2015-03-05 23:57:37 -0800

    Cleanup and style

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,43 +1,61 @@
 'use strict';
 
+Number.prototype.pad = function(size) {
+  var s = String(this);
+  while (s.length < (size || 2)) {s = "0" + s;}
+  return s;
+}
+
 angular.module('rappleApp')
-  .controller('MainCtrl', function ($scope, $http, $interval) {
-  $scope.applicants = ['Jon Doe' ,
-                       'Jane Doe' ,
-                       'Carole Doe' ,
-                       'Applicant 2' ,
-                       'Applicant 3' ,
-                       'Applicant 4' ,
-                       'Applicant 5' ,
-                       'Applicant 6' ,
-                       'Applicant 7' ,
-                       'Applicant 8'
-                      ];
-  var dateDiffInSeconds= function (a, b) {
-    console.log(a , b);
-    var utc1 = a.getTime();
-    var utc2 = b.getTime();
-    return Math.floor((utc2 - utc1) / 1000);
+.controller('MainCtrl', function ($scope, $http, $interval) {
+  $scope.applicants = [
+    'Applicant 1',
+    'Applicant 2',
+    'Applicant 3',
+    'Applicant 4',
+    'Applicant 5'
+  ];
+
+  var dateDiffInSeconds = function (a, b) {
+    return Math.floor((b - a) / 1000);
   }
 
-
   var today = new Date();
-  var resultsDate = new Date(Date.UTC(2015, 2, 6, 20, 0 , 0)); // results will be on March 6 at noon PST or 8 pm UTC
+  var resultsDate = new Date(Date.UTC(2015, 2, 6, 20, 30 , 0)); // results will be on March 6 at 12:30 pm PST - 8:30 pm UTC
 
-  $scope.counter= dateDiffInSeconds(today, resultsDate);
-  var interval = $interval( function(){
-    if ($scope.counter === 0){
+  var clock = function () {
+    var delta = $scope.counter;
+    var days = Math.floor(delta / 86400);
+    delta -= days * 86400;
+    var hours = Math.floor(delta / 3600);
+    delta -= hours * 3600;
+    var minutes = Math.floor(delta / 60);
+    delta -= minutes * 60;
+    var seconds = Math.floor(delta);
+
+    $scope.days = days.pad();
+    $scope.hours = hours.pad();
+    $scope.minutes = minutes.pad();
+    $scope.seconds = seconds.pad();
+  }
+
+  $scope.counter = dateDiffInSeconds(today, resultsDate);
+  $scope.winner = "...."
+  $scope.runnerUp = "...."
+
+  clock();
+
+  var interval = $interval( function() {
+    if ($scope.counter === 0) {
       $interval.cancel(interval);
       $http.get('/api/getRandom?max=10').success(function(response) {
-        console.log(response);
         $scope.winner = $scope.applicants[response.winnerIndex];
         $scope.runnerUp = $scope.applicants[response.runnerUpIndex];
       });
-
       return ;
     }
     $scope.counter--;
+    clock();
   }, 1000);
-
 
 });
--- server/api/index.js
+++ server/api/index.js
@@ -2,23 +2,26 @@ var express = require('express');
 
 var Router = express.Router();
 
-var runnerUpIndex, winnerIndex ;
+var finalPick = false, winnerIndex, runnerUpIndex;
 
 function maxRandomize(max){
   return Math.floor(Math.random() * max);
 }
 
-Router.get('/getRandom' , function(req, res){
-  if( !runnerUpIndex || !winnerIndex){
+Router.get('/getRandom' , function(req, res) {
+  if ( !finalPick ) {
     console.log('generating new randoms');
     var max = parseInt(req.query.max);
-    winnerIndex= maxRandomize(max);
+    winnerIndex = maxRandomize(max);
     runnerUpIndex = maxRandomize(max);
-    while(runnerUpIndex === winnerIndex){
+    while (runnerUpIndex === winnerIndex) {
       runnerUpIndex = maxRandomize(max);
     }
-  }
-  else {
+
+    if ( (new Date()).getTime() >= 1425673800000 ) { // Lock the result at the pick time
+      finalPick = true
+    }
+  } else {
     console.log('using past randoms');
   }
   res.json({winnerIndex: winnerIndex , runnerUpIndex: runnerUpIndex })
