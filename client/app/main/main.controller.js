16f3047b951d6e7f76cc1b85be0bce19c528b22c
Samer Buna <samer@on-site.com>
2015-03-05 17:00:55 -0800

    interval counters and conditional hit to get /randomWinner

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,20 +2,27 @@
 
 angular.module('rappleApp')
   .controller('MainCtrl', function ($scope, $http, $interval) {
-  var today = new Date();
-  var resultsDate = new Date(today.getFullYear(), 3, 6);
-
-  $scope.counter= Math.ceil((resultsDate.getTime()-today.getTime())/1000)
-
-  var interval = $interval( function(){
-    $scope.counter--;
-  }, 1000);
   $scope.applicants = [ 'Aziz' , 'his bro ' , 'Samer' , ' Samer 2',  'Aziz' , 'his bro ' , 'Samer' , ' Samer 2',  'Aziz' , 'his bro '];
 
-  $http.get('/api/getRandom?max=10').success(function(response) {
-    console.log(response);
-    $scope.winner = $scope.applicants[response.winnerIndex];
-    $scope.runnerUp = $scope.applicants[response.runnerUpIndex];
-  });
+
+  var today = new Date();
+  var resultsDate = new Date(today.getFullYear(), 3, 5, 17, 40 , 40);
+
+  $scope.counter= Math.ceil((resultsDate.valueOf()-today.valueOf())/1000)
+
+  var interval = $interval( function(){
+    if ($scope.counter === 0){
+      $interval.cancel(interval);
+      $http.get('/api/getRandom?max=10').success(function(response) {
+        console.log(response);
+        $scope.winner = $scope.applicants[response.winnerIndex];
+        $scope.runnerUp = $scope.applicants[response.runnerUpIndex];
+      });
+      
+      return ;
+    }
+    $scope.counter--;
+  }, 1000);
+
 
 });
