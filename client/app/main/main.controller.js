baa6bc9d2582aa728ca1ff0ffea4b7a1dc0ed296
Samer Buna <samer@on-site.com>
2015-03-05 16:49:04 -0800

    timer till results date

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,13 +1,21 @@
 'use strict';
 
 angular.module('rappleApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $scope.applicants = [ 'Aziz' , 'his bro ' , 'Samer' , ' Samer 2',  'Aziz' , 'his bro ' , 'Samer' , ' Samer 2',  'Aziz' , 'his bro '];
+  .controller('MainCtrl', function ($scope, $http, $interval) {
+  var today = new Date();
+  var resultsDate = new Date(today.getFullYear(), 3, 6);
 
-    $http.get('/api/getRandom?max=10').success(function(response) {
-      console.log(response);
-      $scope.winner = $scope.applicants[response.winnerIndex];
-      $scope.runnerUp = $scope.applicants[response.runnerUpIndex];
-    });
+  $scope.counter= Math.ceil((resultsDate.getTime()-today.getTime())/1000)
 
+  var interval = $interval( function(){
+    $scope.counter--;
+  }, 1000);
+  $scope.applicants = [ 'Aziz' , 'his bro ' , 'Samer' , ' Samer 2',  'Aziz' , 'his bro ' , 'Samer' , ' Samer 2',  'Aziz' , 'his bro '];
+
+  $http.get('/api/getRandom?max=10').success(function(response) {
+    console.log(response);
+    $scope.winner = $scope.applicants[response.winnerIndex];
+    $scope.runnerUp = $scope.applicants[response.runnerUpIndex];
   });
+
+});
