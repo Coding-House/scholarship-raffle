c564f66cc34f1047585f28077b59f33153fc7b82
Samer Buna <samer@on-site.com>
2015-03-05 17:37:20 -0800

    fix diff in seconds

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,14 +2,29 @@
 
 angular.module('rappleApp')
   .controller('MainCtrl', function ($scope, $http, $interval) {
-  $scope.applicants = [ 'Aziz' , 'his bro ' , 'Samer' , ' Samer 2',  'Aziz' , 'his bro ' , 'Samer' , ' Samer 2',  'Aziz' , 'his bro '];
+  $scope.applicants = ['Jon Doe' ,
+                       'Jane Doe' ,
+                       'Carole Doe' ,
+                       'Applicant 2' ,
+                       'Applicant 3' ,
+                       'Applicant 4' ,
+                       'Applicant 5' ,
+                       'Applicant 6' ,
+                       'Applicant 7' ,
+                       'Applicant 8'
+                      ];
+  var dateDiffInSeconds= function (a, b) {
+    console.log(a , b);
+    var utc1 = a.getTime();
+    var utc2 = b.getTime();
+    return Math.floor((utc2 - utc1) / 1000);
+  }
 
 
   var today = new Date();
-  var resultsDate = new Date(today.getFullYear(), 3, 5, 17, 40 , 40);
+  var resultsDate = new Date(Date.UTC(2015, 2, 6, 20, 0 , 0)); // results will be on March 6 at noon PST or 8 pm UTC
 
-  $scope.counter= Math.ceil((resultsDate.valueOf()-today.valueOf())/1000)
-
+  $scope.counter= dateDiffInSeconds(today, resultsDate);
   var interval = $interval( function(){
     if ($scope.counter === 0){
       $interval.cancel(interval);
@@ -18,7 +33,7 @@ angular.module('rappleApp')
         $scope.winner = $scope.applicants[response.winnerIndex];
         $scope.runnerUp = $scope.applicants[response.runnerUpIndex];
       });
-      
+
       return ;
     }
     $scope.counter--;
